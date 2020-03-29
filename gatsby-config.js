eb6017bd90afcc9fe4497340881a30a5b339f96b
Raul Melo <melo.raulf@gmail.com>
2020-03-29 11:27:26 +0200

    test netilify

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
         name: `blog`,
       },
     },
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
     `gatsby-transformer-sharp`,
