95432e3daf35eec2137b3ccb61648ce49f3c3ac3
Sergio Júnior <sergioamjr91@gmail.com>
2020-04-03 18:20:28 -0300

    Using PostContentWrapper on blog-post

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,54 +1,10 @@
 import React from 'react';
-import Img from 'gatsby-image';
-import styled from 'styled-components';
 import rehypeReact from 'rehype-react';
-import media from 'styled-media-query';
-import { motion } from 'framer-motion';
-import { defineMessages } from 'react-intl';
-
-import { Quote, pageTransitionVariants } from '../components/Ui';
-import { BlogGlobalStyle } from '../styles/blogPost';
-import { GlobalStyles } from '../styles';
-import { Container } from '../components/Ui';
-import { MenuBar } from '../components/MenuBar';
+import { Quote } from '../components/Ui';
 import { Gif } from '../components/Gif';
-import { SeriesMenu } from '../components/Blog/SeriesMenu';
 import { Frontmatter, SeriesType, PostSeries, PostEdge } from '../types';
-import SEO from '../components/SEO';
-import { useIntl } from '../context/react-intl';
 import { YouTubeVideo } from '../components/YouTubeVideo';
-import { ThemeProvider } from '../config/theme';
-
-const Title = styled.h1`
-  font-size: 34px;
-  line-height: 40px;
-
-  ${media.greaterThan('medium')`
-    font-size: 42px;
-  `}
-`;
-
-const Subtitle = styled.p`
-  font-size: 24px;
-  line-height: 1.22;
-  letter-spacing: -0.012em;
-  font-family: ${({ theme }) => theme.font.contentSans};
-  opacity: 0.54;
-
-  ${media.greaterThan('medium')`
-    font-size: 28px;
-  `}
-`;
-
-const StyledImg = styled(Img)`
-  max-height: 600px;
-  margin-top: 42px;
-`;
-
-const ImgWrapper = styled(Container)`
-  max-width: 1192px;
-  padding-bottom: 50px;
-`;
+import PostContentWrapper from './post-content-wrapper';
 
 type PostProps = {
   pageContext: {
@@ -64,14 +20,7 @@ const renderAst = new rehypeReact({
   components: { 'big-quote': Quote, gif: Gif, yt: YouTubeVideo },
 }).Compiler;
 
-const messages = defineMessages({
-  featuredImageLabel: {
-    id: 'blog.featuredImage',
-  },
-});
-
 const Post: React.FC<PostProps> = ({ pageContext }) => {
-  const { formatMessage } = useIntl();
   React.useEffect(() => {
     /* This loads all widgets from twitter if exists. 
     It's loaded by html.tsx (data-testid="twitter-script")
@@ -94,69 +43,20 @@ const Post: React.FC<PostProps> = ({ pageContext }) => {
     series: seriesInfo,
   } = frontmatter as Frontmatter;
 
-  const SeriesSection: React.FC<{ noDivider?: boolean }> = ({
-    noDivider = false,
-  }) => {
-    return (
-      series && (
-        <>
-          {!noDivider && <hr />}
-          <Container as="section">
-            <SeriesMenu
-              series={series}
-              postIndex={seriesInfo!.index}
-              title={title}
-            />
-          </Container>
-        </>
-      )
-    );
-  };
-
   return (
-    <>
-      <SEO
-        title={title}
-        description={description || excerpt!}
-        lang={fields?.lang!}
-        url={fields?.slug!}
-        image={image?.publicURL}
-      />
-      <ThemeProvider>
-        <motion.div
-          initial="exit"
-          animate="enter"
-          exit="exit"
-          variants={pageTransitionVariants}
-        >
-          <GlobalStyles />
-          <BlogGlobalStyle />
-          <MenuBar />
-          <Container as="header">
-            <Title>{title}</Title>
-            {subtitle && <Subtitle>{subtitle}</Subtitle>}
-          </Container>
-          <SeriesSection noDivider />
-          {image && (
-            <ImgWrapper
-              role="img"
-              aria-label={formatMessage(messages.featuredImageLabel)}
-            >
-              <StyledImg
-                fluid={image.childImageSharp.fluid}
-                alt={imageCaption}
-              />
-              {imageCaption && <p className="img-caption">{imageCaption}</p>}
-            </ImgWrapper>
-          )}
-          <Container className="post" as="main">
-            {renderAst(htmlAst)}
-            <SeriesSection />
-          </Container>
-          <br />
-        </motion.div>
-      </ThemeProvider>
-    </>
+    <PostContentWrapper
+      series={series}
+      title={title}
+      seriesInfo={seriesInfo}
+      image={image}
+      image_caption={imageCaption}
+      subtitle={subtitle}
+      lang={fields?.lang!}
+      url={fields?.slug!}
+      description={description || excerpt!}
+    >
+      {renderAst(htmlAst)}
+    </PostContentWrapper>
   );
 };
 
