c3ed66cccbfec90fa541151c744b88b803258bc8
Raul Melo <melo.raulf@gmail.com>
2020-05-30 13:53:24 +0200

    fix mdx compnent names

--- src/components/MdxComponents/index.ts
+++ src/components/MdxComponents/index.ts
@@ -3,7 +3,7 @@ import { Quote } from './Quote';
 import { YouTubeVideo } from './YouTubeVideo';
 
 export const blogComponents = {
-  'big-quote': Quote,
-  gif: Gif,
-  yt: YouTubeVideo,
+  BigQuote: Quote,
+  Gif,
+  YouTubeVideo,
 };
