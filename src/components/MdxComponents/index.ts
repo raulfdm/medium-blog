12afa2d125828541beb9b434a88f1314c03d8236
Raul Melo <melo.raulf@gmail.com>
2020-05-30 10:43:19 +0200

    create mdx components folder and move its components there

---
+++ src/components/MdxComponents/index.ts
@@ -0,0 +1,9 @@
+import { Gif } from './Gif';
+import { Quote } from './Quote';
+import { YouTubeVideo } from './YouTubeVideo';
+
+export const blogComponents = {
+  'big-quote': Quote,
+  gif: Gif,
+  yt: YouTubeVideo,
+};
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import rehypeReact from 'rehype-react';
 import { motion } from 'framer-motion';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import { Header } from 'components/Blog/Header';
 import { FeaturedImage } from 'components/Blog/FeaturedImage';
@@ -8,29 +8,24 @@ import { SeriesSection } from 'components/Blog/SeriesSection';
 import { BlogPostProps } from 'components/Blog/types';
 import { BlogContextProvider } from 'components/Blog/blogContext';
 import { useTwitterScript } from 'components/Blog/useTwitterScript';
-import { Quote, pageTransitionVariants } from '../components/Ui';
+import { pageTransitionVariants } from '../components/Ui';
 import { BlogGlobalStyle } from '../styles/blogPost';
 import { GlobalStyles } from '../styles';
 import { Container } from '../components/Ui';
 import { MenuBar } from '../components/MenuBar';
-import { Gif } from '../components/Blog/Gif';
 import { Frontmatter } from '../types';
 import SEO from '../components/SEO';
-import { YouTubeVideo } from '../components/YouTubeVideo';
 import { ThemeProvider } from '../context/theme';
 
-/* Custom Components */
-const renderAst = new rehypeReact({
-  createElement: React.createElement,
-  components: { 'big-quote': Quote, gif: Gif, yt: YouTubeVideo },
-}).Compiler;
+import { blogComponents } from '../components/MdxComponents';
 
 const BlogPost: React.FC<BlogPostProps> = ({ pageContext }) => {
   useTwitterScript();
 
   const { series, post } = pageContext;
 
-  const { htmlAst, frontmatter, excerpt, fields } = post.node;
+  const { body, frontmatter, excerpt, fields } = post.node;
+  console.log(post.node);
   const {
     image,
     image_caption: imageCaption,
@@ -66,7 +61,7 @@ const BlogPost: React.FC<BlogPostProps> = ({ pageContext }) => {
             <SeriesSection noDivider />
             <FeaturedImage />
             <Container className="post" as="main">
-              {renderAst(htmlAst)}
+              <MDXRenderer components={blogComponents}>{body}</MDXRenderer>
               <SeriesSection />
             </Container>
           </BlogContextProvider>
